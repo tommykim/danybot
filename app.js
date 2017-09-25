13c817d571d06933d91964246a834a70b293a1c4
KWAGDAEYONG <dayong7387@hanmail.net>
2017-09-25 15:55:15 +0900

    btn t

--- app.js
+++ app.js
@@ -205,7 +205,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendTextMessage(senderID, payload);
 }
 function sendTextMessage(recipientId, messageText) {
   var messageData = {
