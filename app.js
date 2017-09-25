dec59a326188b0f2158fa283a03261a9aa693c82
KWAGDAEYONG <dayong7387@hanmail.net>
2017-09-25 13:07:08 +0900

    dany bot

--- app.js
+++ app.js
@@ -15,6 +15,65 @@ app.get('/webhook', function(req, res) {
     }
     res.send('Error, wrong token');
 })
+
+app.post("/webhook", function(req,res){
+	console.log("WEBHOOK GET IT WORKS");
+	var data = req.body;
+	console.log(data);
+	
+	if(data.object =='page){
+		data.entry.forEach(function(pageEntry){
+			var pageID = pageEntry.id;
+			var timeOfEvent = pageEntry.time;
+			
+			pageEntry.messaging.forEach(function(messagingEvent){
+				if(messagingEvent.optin){
+					receivedAuthentication(messagingEvent);
+				} else if(messagingEvent.message){
+					receivedMessage(messagingEvent);
+				} else if(messagingEvent.postback){
+					receivedPostback(messagingEvent);
+				} else{
+					console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+				}
+			});
+		});
+		res.sendStatus(200);
+	}
+});
+
+function receivedMessage(event) {
+    var senderId = event.sender.id;
+    var content = event.message.text;
+    var echo_message = "ECHO : " + content;
+    sendTextMessage(senderId, echo_message);
+}
+function receivedPostback(event) {
+    console.log("RECEIVED POSTBACK IT WORKS");
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+    var payload = event.postback.payload;
+    console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, payload, timeOfPostback);
+    sendTextMessage(senderID, "Postback called");
+}
+function sendTextMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: recipientId },
+            message: { text: message }
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ' + response.error);
+        }
+    });
+}
+
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
 })
